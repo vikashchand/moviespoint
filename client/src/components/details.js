7aa1882bbef9395b1d9dd727e3b2022499f20acd
vikash chand <vikash.chand2020@vitstudent.ac.in>
2024-06-01 14:37:55 +0530

    new changesssgfdbvcngfs

--- client/src/components/details.js
+++ client/src/components/details.js
@@ -148,7 +148,7 @@ const MovieInfo = () => {
   return (
     <div className="maincontainer">
     <Navbar/>
-    <div className="container mt-8">
+    <div className="containers mt-8">
       {movieData && (
         <div className="row">
           <div className="col-md-4">
